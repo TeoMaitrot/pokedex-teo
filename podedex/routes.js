b27fc47ca0a64458c2360ff1b9d073d8a1cdc36a
Teo21maitr <95919711+Teo21maitr@users.noreply.github.com>
2024-05-30 20:47:21 +0200

    Ajout d'une méthode pour récuper uniquement un seul pokemon avec son id

--- podedex/routes.js
+++ podedex/routes.js
@@ -18,4 +18,17 @@ router.post("/pokemons", async (req, res) => {
 	res.send(pokemon)
 })
 
+// Récupérer un seul pokemon
+router.get("/pokemons/:id", async (req, res) => {
+    try {
+        const pokemon = await Pokemon.findOne({ id: req.params.id });
+        if (!pokemon) {
+            return res.status(404).json({ error: "Le pokemon avec l'ID " + req.params.id + " n'existe pas" });
+        }
+        res.send(pokemon);
+    } catch (error) {
+        res.status(500).json({ error: "Une erreur est survenue lors de la récupération du pokemon" });
+    }
+});
+
 module.exports = router
