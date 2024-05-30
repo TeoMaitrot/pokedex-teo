793c23dea3d20dc9bb0b596afb3609aaedba67a6
Teo21maitr <95919711+Teo21maitr@users.noreply.github.com>
2024-05-31 00:49:30 +0200

    Ajout des méthodes pour créer un utilisateur et pour créer un utilisateur pour un pokedex

--- podedex/routes.js
+++ podedex/routes.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const Pokemon = require("./models/Pokemon")
+const Utilisateur = require("./models/Utilisateur");
+const Pokedex = require("./models/Pokedex");
 const router = express.Router()
 
 // Récupérer tous les pokemons
@@ -28,6 +30,54 @@ router.get("/pokemons/:id", async (req, res) => {
         res.send(pokemon);
     } catch (error) {
         res.status(500).json({ error: "Une erreur est survenue lors de la récupération du pokemon" });
+    }
+});
+
+// Créer un utilisateur
+router.post("/utilisateurs", async (req, res) => {
+    try {
+        const { id, nom } = req.body;
+
+        const utilisateur = new Utilisateur({
+            id,
+            nom,
+            pokedexes: [],
+            equipe: []
+        });
+
+        await utilisateur.save();
+
+        res.send(utilisateur);
+    } catch (error) {
+        res.status(500).send({ error: "Erreur lors de la création de l'utilisateur" });
+    }
+});
+
+// Création d'un pokedex pour un utilisateur
+router.post("/utilisateurs/:utilisateurId/pokedexes", async (req, res) => {
+    try {
+        const utilisateur = await Utilisateur.findOne({ id: req.params.utilisateurId });
+        const { id, nom } = req.body;
+
+        if (!utilisateur) {
+            return res.status(404).send({ error: "Utilisateur non trouvé" });
+        }
+
+        const pokedex = new Pokedex({
+            id,
+            nom,
+            utilisateur: utilisateur._id,
+            pokemons: []
+        });
+
+        await pokedex.save();
+
+        utilisateur.pokedexes.push(pokedex._id);
+        await utilisateur.save();
+
+        res.send(pokedex);
+    } catch (error) {
+        res.status(500).send({ error: "Erreur lors de la création du Pokédex" });
     }
 });
 
