54ff324f7130ae2b36f74c04479c7a37adf96bb6
Teo21maitr <95919711+Teo21maitr@users.noreply.github.com>
2024-05-30 19:34:31 +0200

    Ajout de la connexion entre le serveur et la base de donnée distante sur MongoDB

--- podedex/index.js
+++ podedex/index.js
@@ -1,7 +1,24 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
+
+// URL pour se connecter à ta base de données MongoDB
+const mongoAtlasUri = "mongodb+srv://teo21maitr:admin@cluster0.ztutran.mongodb.net/pokedex?retryWrites=true&w=majority";
+
+// Fonction asynchrone pour se connecter à MongoDB
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(mongoAtlasUri);
+        console.log("Mongoose est connecté");
+    } catch (e) {
+        console.error("La connexion a échoué", e);
+    }
+}
+
+// Appel de la fonction pour se connecter à la base de données
+connectToDatabase();
 
 app.listen(5000, () => {
-	console.log("Helow world")
-})
+    console.log("Le serveur a démarré !");
+});
